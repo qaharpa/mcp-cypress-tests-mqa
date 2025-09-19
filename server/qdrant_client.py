099b8d7937db1d037a08e4f3303ed445b8650c9d
Muhammad Qasim <108104679+qaharpa@users.noreply.github.com>
2025-09-19 19:26:26 +1000

    Delete server directory

--- server/qdrant_client.py
+++
@@ -1,8 +0,0 @@
-from qdrant_client import QdrantClient
-from config.settings import settings
-
-def get_qdrant_client():
-    return QdrantClient(
-        host=settings["qdrant"]["host"],
-        port=settings["qdrant"]["port"]
-    )
